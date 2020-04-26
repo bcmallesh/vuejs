703079afb625a68f79e00e2cc24e3c3086ede025
Chinna Mallesh Boya <chinna.boya@evry.com>
2020-04-27 04:55:34 +0530

    computed

--- practice-twolwaybinding.js
+++ practice-twolwaybinding.js
@@ -3,11 +3,20 @@ new Vue({
 el:'#practice-twolwaybinding',
 data:{
 name :'Chinna Mallesh',
-counter:0
+counter:0,
+secondCounter:0
+},
+computed:{
+output:function(){
+	console.log('computed');
+return this.counter>5?'greater than 5':'less than 5'	
+}	
+	
 },
 methods:{
-result(){
-	
+result:function(){
+		console.log('method');
+
 	return this.counter>5?'greater than 5':'less than 5'
 }
 	
