4285665e296e9a972de02b5298ed8cdb430a433f
Chinna Mallesh Boya <chinna.boya@evry.com>
2020-04-27 05:20:33 +0530

    short cuts for events

--- practice.js
+++ practice.js
@@ -2,31 +2,19 @@ new Vue({
 
 el:'#practice',
 data:{
-name :'Chinna Mallesh',
-counter:0,
-secondCounter:0
-},
-computed:{
-output:function(){
-	console.log('computed');
-return this.counter>5?'greater than 5':'less than 5'	
-}	
-	
-},
-methods:{
-result:function(){
-		console.log('method');
 
-	return this.counter>5?'greater than 5':'less than 5'
+
+link:'http://google.com'
+},
+	
+
+methods:{
+changeLink:function(){
+		
+
+	this.link='http://apple.com'
 }
 	
-},
-watch:{
-	counter:function(value){
-		var vm=this;
-		setTimeout(function(){
-			vm.counter=0;
-		},4000);
-	}
 }
+
 });
