f95689366eaf3e1dd9df1cd54fac3a66b12842b3
Chinna Mallesh Boya <chinna.boya@evry.com>
2020-04-27 06:44:52 +0530

    dynamic styling with css using names

--- css.js
+++ css.js
@@ -1,22 +1,18 @@
-new Vue({
-
-el:'#practice',
-data:{
-attachRed:false
-},
-computed:{
-divClasses:function(){
-	return {
-		red:this.attachRed,
-		blue:!this.attachRed
-	}
-}
-},
-methods:{
-
-	
-},
-watch:{
-	
-}
-});
+ new Vue({
+        el: '#practice',
+        data: {
+            attachRed: false,
+            color: 'green'
+           
+        },
+        computed: {
+ 
+            divClasses: function () {
+                return {
+                    red: this.attachRed,
+                    blue: !this.attachRed
+                };
+ 
+            }
+         }
+    })
