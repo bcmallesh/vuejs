a5f50aa0287ca85d8c4995f1f75fe88518d7830b
Chinna Mallesh Boya <chinna.boya@evry.com>
2020-04-28 03:35:14 +0530

    v-for with objects

--- conditional.js
+++ conditional.js
@@ -5,7 +5,7 @@ data:{
 show:true,
 
 ingradients:['meat','fruit','cookies'],
-persons:[{name:'chinna',age:33,desgnation:'private employee'},{name:'maheswari',age:26,desgnation:'house wife'},{name:'nanda kishore',age:5,desgnation:'student'},{name:'vishnavi',age:3,desgnation:'student'},{name:'jaikrishna',age:1,desgnation:'unknown'}]
+persons:[{name:'chinna mallesh',age:34,desgnation:'private employee'},{name:'maheswari',age:26,desgnation:'house wife'},{name:'nanda kishore',age:5,desgnation:'student'},{name:'vishnavi',age:3,desgnation:'student'},{name:'jaikrishna',age:1,desgnation:'student'}]
 },
 computed:{
 
