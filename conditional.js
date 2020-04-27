8fb4c092421fa5ab4f4b82e2ca572cf8016866dc
Chinna Mallesh Boya <chinna.boya@evry.com>
2020-04-28 03:19:03 +0530

    v-for

--- conditional.js
+++ conditional.js
@@ -2,7 +2,10 @@ new Vue({
 
 el:'#conditional',
 data:{
-show:true
+show:true,
+
+ingradients:['meat','fruit','cookies'],
+persons:[{name:'chinna',age:33,desgnation:'private employee'},{name:'maheswari',age:26,desgnation:'house wife'},{name:'nanda kishore',age:5,desgnation:'student'},{name:'vishnavi',age:3,desgnation:'student'},{name:'jaikrishna',age:1,desgnation:'unknown'}]
 },
 computed:{
 
